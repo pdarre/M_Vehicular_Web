dccb9cdbb9658c2bee958891be6b1f210ac5723e
pdarre <pdarre.ort@gmail.com>
2019-04-01 17:01:02 -0300

    add vehiculo api, getVehiculos()

---
+++ M_Vehicular_Web/Controllers/API/VehiculosController.cs
@@ -0,0 +1,23 @@
+﻿namespace M_Vehicular_Web.Controllers.API
+{
+    using M_Vehicular_Web.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculosController : Controller
+    {
+        private readonly IVehiculoRepository vehiculoRepository;
+
+        public VehiculosController(IVehiculoRepository vehiculoRepository)
+        {
+            this.vehiculoRepository = vehiculoRepository;
+        }
+
+        [HttpGet]
+        public ActionResult GetVehiculos()
+        {
+            return Ok(this.vehiculoRepository.GetAll());
+        }
+    }
+}
